45f15090d5934705dfda1e979963c5b2b65b0d4a
Alex Helfet <alex.helfet@gmail.com>
2021-11-20 21:03:03 +0000

    Insert WindowDescriptor for window size and name.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ use bevy::prelude::*;
 
 fn main() {
     App::build()
+        .insert_resource(WindowDescriptor {
+            title: "One-Click Ninja".to_string(),
+            width: 800.,
+            height: 600.,
+            .. Default::default()
+        })
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
         .run();
