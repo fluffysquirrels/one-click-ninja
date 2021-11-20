e8d3d65ba69b878b30932dd625d015e29c998b84
Alex Helfet <alex.helfet@gmail.com>
2021-11-20 21:28:35 +0000

    Add sword sprite for actions wheel.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use bevy::prelude::*;
 
+const WIN_W: f32 = 800.;
+const WIN_H: f32 = 600.;
+
 fn main() {
     App::build()
         .insert_resource(WindowDescriptor {
             title: "One-Click Ninja".to_string(),
-            width: 800.,
-            height: 600.,
+            width: WIN_W,
+            height: WIN_H,
             .. Default::default()
         })
         .add_plugins(DefaultPlugins)
@@ -15,6 +18,18 @@ fn main() {
 
 fn setup(
     mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-
+    let tex = asset_server.load("sword.png");
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(tex.into()),
+        transform: Transform {
+            translation: Vec3::new(-200., 100., 0.),
+            scale: Vec3::new(0.3, 0.3, 0.3),
+            .. Default::default()
+        },
+        .. Default::default()
+    });
 }
