3bd6ec8eaf05d6fe5af9fb2dc061b555e9761fa5
Alex Helfet <alex.helfet@gmail.com>
2021-11-23 13:56:31 +0000

    Show player sprite, show dead when dead.

--- src/player.rs
+++ src/player.rs
@@ -8,19 +8,41 @@ pub struct Plugin;
 
 struct PlayerHpDisplay;
 
-const START_HP: Hp = 10;
+const START_HP: Hp = 3;
+
+struct Sprites {
+    idle: Handle<ColorMaterial>,
+    attack: Handle<ColorMaterial>,
+    dead: Handle<ColorMaterial>,
+}
 
 impl bevy::app::Plugin for Plugin {
     fn build(&self, app: &mut AppBuilder) {
         app
+            .add_startup_system_to_stage(StartupStage::PreStartup, load_resources.system())
             .add_startup_system(spawn_player_hp.system())
             .add_startup_system(spawn_player.system())
             .add_system(update_player_hp.system());
     }
 }
 
+fn load_resources(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.insert_resource(Sprites {
+        idle: materials.add(
+            asset_server.load("sprites/lpc-medieval-fantasy-character/our_work/player/walk_up/00.png").into()),
+        attack: materials.add(
+            asset_server.load("sprites/lpc-medieval-fantasy-character/our_work/player/spear_up/05.png").into()),        dead: materials.add(
+            asset_server.load("sprites/lpc-medieval-fantasy-character/our_work/player/die/05.png").into()),
+    });
+}
+
 fn spawn_player(
     mut commands: Commands,
+    sprites: Res<Sprites>,
 ) {
     commands
         .spawn()
@@ -28,6 +50,15 @@ fn spawn_player(
         .insert(Health {
             current: START_HP,
             max: START_HP,
+        })
+        .insert_bundle(SpriteBundle {
+            material: sprites.idle.clone(),
+            transform: Transform {
+                translation: Vec3::new(200., 0., 0.),
+                scale: Vec3::ONE * 2.0,
+                .. Default::default()
+            },
+            .. Default::default()
         });
 }
 
@@ -48,16 +79,24 @@ fn spawn_player_hp(
                 horizontal: HorizontalAlign::Center,
             },
         ),
+        transform: Transform {
+            translation: Vec3::new(200., -200., 0.),
+            .. Default::default()
+        },
         .. Default::default()
     }).insert(PlayerHpDisplay);
 }
 
 fn update_player_hp(
-    player: Query<(&Player, &Health)>,
-    mut display: Query<(&PlayerHpDisplay, &mut Text)>,
+    mut player: Query<(&Player, &Health, &mut Handle<ColorMaterial>)>,
+    mut display_hp: Query<(&PlayerHpDisplay, &mut Text)>,
+    sprites: Res<Sprites>,
 ) {
-    for (_, health) in player.single() {
-        for (_, mut text) in display.single_mut() {
+    for (_, health, mut mat) in player.single_mut() {
+        if health.current == 0 {
+            *mat = sprites.dead.clone();
+        }
+        for (_, mut text) in display_hp.single_mut() {
             text.sections[0].value = format_hp(health.current, health.max);
         }
     }
