fbd777bf30629352954d576c494ef9af68896920
Alex Helfet <alex.helfet@gmail.com>
2021-11-23 13:55:24 +0000

    Fix enemy startup to use load_resources system.

--- src/enemy.rs
+++ src/enemy.rs
@@ -16,15 +16,13 @@ struct AttackAnimation {
     until: std::time::Duration,
 }
 
-const LOAD: &'static str = "load";
-
 pub const ATTACK_DURATION: Duration = Duration::from_millis(300);
 
 impl bevy::app::Plugin for Plugin {
     fn build(&self, app: &mut AppBuilder) {
         app
-            .add_startup_system(load_resources.system().label(LOAD))
-            .add_startup_system(spawn_current_enemy.system().after(LOAD))
+            .add_startup_system_to_stage(StartupStage::PreStartup, load_resources.system())
+            .add_startup_system(spawn_current_enemy.system())
             .add_system(enemy_attack.system())
             .add_system(attack_animation.system());
     }
@@ -43,18 +41,14 @@ fn load_resources(
     });
 }
 
-/// This should use the sprite loaded in load_resources, but even with the
-/// system ordering using labels, the Sprites resource is not available when
-/// this startup system runs.
 fn spawn_current_enemy(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut materials: ResMut<Assets<ColorMaterial>>,
-    // sprites: Res<Sprites>,
+    sprites: Res<Sprites>,
 ) {
     commands.spawn_bundle(SpriteBundle {
-        material: materials.add(
-            asset_server.load("sprites/Samurai02/01-Idle/01-Normal/2D_SM02_Idle_000.png").into()),
+        material: sprites.idle.clone(),
         transform: Transform {
             translation: Vec3::new(200., 200., 0.),
             scale: Vec3::new(0.3, 0.3, 0.3),
